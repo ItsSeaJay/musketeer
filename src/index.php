27da9a08769f78736b8a679f6330a5866fff62ff
Unknown <b6010572@my.shu.ac.uk>
2018-09-12 17:00:26 +0000

    Obtained release numbers from the internet

--- src/index.php
+++ src/index.php
@@ -1,2 +1,9 @@
 <?php
-echo 'Hello, World!';
+// Obtain the list of CodeIgniter releases as an Atom feed
+$feed = simplexml_load_file('https://github.com/bcit-ci/CodeIgniter/releases.atom');
+$releases = array();
+
+foreach ($feed->entry as $entry)
+{
+	array_push($releases, $entry->title);
+}
