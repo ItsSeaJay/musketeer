7352d8a270ffd9650e9ace6890536601841e4027
Unknown <b6010572@my.shu.ac.uk>
2018-09-12 17:26:15 +0000

    Added an extra gitignore

--- src/index.php
+++ src/index.php
@@ -1,9 +1,24 @@
 <?php
 // Obtain the list of CodeIgniter releases as an Atom feed
 $feed = simplexml_load_file('https://github.com/bcit-ci/CodeIgniter/releases.atom');
-$releases = array();
 
-foreach ($feed->entry as $entry)
+// Use the latest release number to construct the download URL
+// (assuming that each entry is in chronological order with the newest first)
+// e.g. https://github.com/bcit-ci/CodeIgniter/archive/3.1.9.zip
+$latest_version = $feed->entry[0]->title;
+$download_url = 'https://github.com/bcit-ci/CodeIgniter/archive/'.$latest_version.'.zip';
+
+// Download that version of CodeIgniter to the server as a `.zip` file
+$file_name = 'CodeIgniter-'.$latest_version.'.zip';
+file_put_contents($file_name, fopen($download_url, 'r'));
+
+// Unzip the contents of that file to the root
+$zip = new ZipArchive;
+
+if (($zip->open($file_name)) === TRUE)
 {
-	array_push($releases, $entry->title);
+	$zip->extractTo('');
+	$zip->close();
+
+	echo 'Extracted zip archive';
 }
