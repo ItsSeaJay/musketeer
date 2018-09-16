2d58254719913673208c6b36e137cfa147979ffe
Unknown <b6010572@my.shu.ac.uk>
2018-09-16 18:24:29 +0000

    Added build script

--- src/installer.php
+++ src/installer.php
@@ -1,4 +1,5 @@
 <?php
+// Define some utility functions to be used by the installer
 function recursive_copy($source, $destination)
 {
 	$dir = opendir($source);
@@ -47,75 +48,107 @@ function recursive_delete($source)
     rmdir($source);
 }
 
+/**
+ * An installer for the CodeIgniter framework
+ */
+class Installer {
+
+	/**
+	* Installs the CodeIgniter framework to the given destination
+	*/
+	public function install($destination = '')
+	{
+		// Use the latest release number to construct the download URL
+		// (assuming that each entry is in chronological order with the newest first)
+		// e.g. https://github.com/bcit-ci/CodeIgniter/archive/3.1.9.zip
+		$latest_version = $this->get_latest_version();
+		$download_url = 'https://github.com/bcit-ci/CodeIgniter/archive/'.$latest_version.'.zip';
+
+		// Download that version of CodeIgniter to the server as a `.zip` file
+		$file_name = 'CodeIgniter-'.$latest_version.'.zip';
+		$folder_name = $desination.'CodeIgniter-'.$latest_version;
+		file_put_contents($file_name, fopen($download_url, 'r'));
+
+		// Unzip the contents of that file to the absolute path
+		$zip = new ZipArchive;
+
+		if (($zip->open($file_name)) === TRUE)
+		{
+			$zip->extractTo($destination);
+			$zip->close();
+		}
+
+		// Get a list of templates needed to complete the installation process
+		$templates = $this->get_templates();
+
+		// Get the site configuration from the previous form
+		$config['base_url'] = $_POST['base_url'] ?? 'http://example.com/';
+
+		// Replace the information in the 'config' template with the user's configuration
+		$templates['config'] = str_replace('{base_url}', $config['base_url'], $templates['config']);
+
+		// Store that information in the appropriate file
+		file_put_contents($folder_name.'application\\config\\config.php', $templates['config']);
+
+		// Do the same for the database configuration
+		$db['username'] = $_POST['db_username'] ?? '';
+		$db['password'] = $_POST['db_password'] ?? '';
+
+		$templates['database'] = str_replace('{username}', $db['username'], $templates['database']);
+		$templates['database'] = str_replace('{password}', $db['password'], $templates['database']);
+
+		file_put_contents($folder_name.'application\\config\\database.php', $templates['database']);
+
+		// Write the index file to the folder root from its template
+		// TODO: ensure the index file is in it's own folder
+		file_put_contents($destination.'index.php', $templates['index']);
+
+		// Move the necessary files into place unless they already exist
+		if (!is_dir($destination.'application') AND !is_dir($destination.'\\system'))
+		{
+			recursive_copy($destination.'CodeIgniter-'.$latest_version.'\\application', $destination.'\\application');
+			recursive_copy($destination.'CodeIgniter-'.$latest_version.'\\system', $destination.'\\system');
+		}
+
+		copy($destination.'CodeIgniter-'.$latest_version.'\\index.php', $destination.'\\index.php');
+
+		// Clean up any excess files left behind by the process
+		recursive_delete($desination.'CodeIgniter-'.$latest_version);
+		unlink($desination.$file_name);
+
+		// Redirect the user to their new site
+		header('Location: index.php');
+	}
+
+	/**
+	* Gets the latest version of CodeIgniter from the Atom releases feed
+	*/
+	private function get_latest_version()
+	{
+		$feed = simplexml_load_file('https://github.com/bcit-ci/CodeIgniter/releases.atom');
+		$latest_version = $feed->entry[0]->title;
+
+		return $latest_version;
+	}
+
+	private function get_templates()
+	{
+		$templates = array();
+		$templates['config'] = file_get_contents($destination.'templates\\config.txt');
+		$templates['database'] = file_get_contents($destination.'templates\\database.txt');
+		$templates['index'] = file_get_contents($destination.'templates\\index.txt');
+
+		return $templates;
+	}
+}
+
 if (isset($_POST['base_url']))
 {
-	$feed = simplexml_load_file('https://github.com/bcit-ci/CodeIgniter/releases.atom');
-
-	// Use the latest release number to construct the download URL
-	// (assuming that each entry is in chronological order with the newest first)
-	// e.g. https://github.com/bcit-ci/CodeIgniter/archive/3.1.9.zip
-	$latest_version = $feed->entry[0]->title;
-	$download_url = 'https://github.com/bcit-ci/CodeIgniter/archive/'.$latest_version.'.zip';
-
-	// Download that version of CodeIgniter to the server as a `.zip` file
-	$file_name = 'CodeIgniter-'.$latest_version.'.zip';
-	$folder_name = dirname(__FILE__).'\\'.'CodeIgniter-'.$latest_version;
-	file_put_contents($file_name, fopen($download_url, 'r'));
-
-	// Unzip the contents of that file to the absolute path
-	$zip = new ZipArchive;
-
-	if (($zip->open($file_name)) === TRUE)
-	{
-		$zip->extractTo(dirname(__FILE__));
-		$zip->close();
-	}
-
-	// Get a list of templates needed to complete the installation process
-	$templates = array();
-	$templates['config'] = file_get_contents(dirname(__FILE__).'\\templates\\config.txt');
-	$templates['database'] = file_get_contents(dirname(__FILE__).'\\templates\\database.txt');
-	$templates['index'] = file_get_contents(dirname(__FILE__).'\\templates\\index.txt');
-
-	// Get the site configuration from the previous form
-	$config['base_url'] = $_POST['base_url'] ?? 'http://example.com/';
-
-	// Replace the information in the 'config' template with the user's configuration
-	$templates['config'] = str_replace('{base_url}', $config['base_url'], $templates['config']);
-
-	// Store that information in the appropriate file
-	file_put_contents($folder_name.'\\application\\config\\config.php', $templates['config']);
-
-	// Do the same for the database configuration
-	$db['username'] = $_POST['db_username'] ?? '';
-	$db['password'] = $_POST['db_password'] ?? '';
-
-	$templates['database'] = str_replace('{username}', $db['username'], $templates['database']);
-	$templates['database'] = str_replace('{password}', $db['password'], $templates['database']);
-
-	file_put_contents($folder_name.'\\application\\config\\database.php', $templates['database']);
-
-	// Write the index file to the folder root from its template
-	// TODO: ensure the index file is in it's own folder
-	file_put_contents(dirname(__FILE__).'\\index.php', $templates['index']);
-
-	// Move the necessary files into place unless they already exist
-	if (!is_dir(dirname(__FILE__).'\\application') AND !is_dir(dirname(__FILE__).'\\system'))
-	{
-		recursive_copy(dirname(__FILE__).'\\CodeIgniter-'.$latest_version.'\\application', dirname(__FILE__).'\\application');
-		recursive_copy(dirname(__FILE__).'\\CodeIgniter-'.$latest_version.'\\system', dirname(__FILE__).'\\system');
-	}
-
-	copy(dirname(__FILE__).'CodeIgniter-'.$latest_version.'\\index.php', dirname(__FILE__).'\\index.php');
-
-	// Clean up any excess files left behind by the process
-	recursive_delete(dirname(__FILE__).'\\'.'CodeIgniter-'.$latest_version);
-	unlink(dirname(__FILE__).'\\'.$file_name);
-
-	// Redirect the user to their new site
-	header('Location: index.php');
+	$installer = new Installer();
+	$installer->install(dirname(__FILE__).'\\');
 }
 ?>
+
 <html>
 	<head>
 		<title>Installer</title>
---
+++ utils/build.py
@@ -0,0 +1,7 @@
+import shutil
+
+templates = {
+	'config' = '../src/templates/config.txt',
+	'database' = '../src/templates/database.txt',
+	'index' = '../src/templates/index.txt'
+}
