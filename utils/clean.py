f23d2ecacef9ef993a1132bf810adbd6fcd65604
Unknown <b6010572@my.shu.ac.uk>
2018-09-16 15:42:31 +0000

    Cleaned the source folder

--- utils/clean.py
+++ utils/clean.py
@@ -1,1 +1,7 @@
-import shutils
+import os
+import shutil
+
+# Clean out the CodeIgniter files present in the source folder
+shutil.rmtree('src/application')
+shutil.rmtree('src/system')
+os.remove('src/index.php')
